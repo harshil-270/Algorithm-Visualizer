a367e5dc92155ef5a0135d47d30ea67f824709f4
harshil-270 <harshiltagadiya@gmail.com>
2020-08-05 00:21:59 +0530

    Uploading work 2

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-var canvas
-var canvasContext
-const n = 15
+let canvas
+let canvasContext
+let n = 100
 let line_width;
+var cnt = 0
+var a = new Array();
+
+
 function randomvalue(min,max){
     return Math.random() * (max - min) + min;
 }
-function draw(a,aa,bb){
-    canvasContext.fillStyle = 'black'
+function draw(a,aa,bb,color){
+    canvasContext.fillStyle = "rgba(35,50,65,1)"
+    // canvasContext.fillStyle = 'black'
     canvasContext.fillRect(0,0,canvas.width,canvas.height)
+    console.log(n)
     for(let ii = 0; ii < n; ii++){
         if(ii == aa || ii == bb){
-            canvasContext.fillStyle = 'red'
+            canvasContext.fillStyle = color
         }
         else{
             canvasContext.fillStyle = 'white'
         }
+        // console.log(a[ii])
         canvasContext.fillRect(ii * line_width,canvas.height - a[ii],line_width - 1 ,a[ii])
     }
 }
-window.onload = function() {
-    console.log('Page Loaded  : ');
+
+var slider = document.getElementById("myRange");
+slider.oninput = function() {
+    document.getElementById('slider_value').innerHTML = slider.value
+    // console.log(slider.value)
+    cnt++
+    n = slider.value
+    line_width = canvas.width / n
+    for(let i = 0; i < n; i++){
+        a[i] = parseInt(randomvalue(1,canvas.height))
+        console.log(a[i])
+    }
+    draw(a,-1,-1,"green")
+}
+
+window.onload = function(){
     canvas = document.getElementById('maincanvas');
     canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black'
+    canvasContext.fillStyle = "rgba(35,50,65,1)"
     canvasContext.fillRect(0,0,canvas.width,canvas.height)
-    let a = new this.Array();
-    for(let i = 0; i < n; i++){
-        a[i] = parseInt(randomvalue(0,canvas.height))
-    }
+
+    n = slider.value
     line_width = canvas.width / n
-    console.log('Line width : ' + line_width)
-    // for(let i = 0; i < n; i++){
-    //     canvasContext.fillStyle = 'white'
-    //     console.log(a[i])
-    //     canvasContext.fillRect(i * line_width,0,line_width ,a[i])
-    // }
-    let i = 0
-    let j = 0
+    for(let i = 0; i < n; i++){
+        a[i] = parseInt(randomvalue(1,canvas.height))
+        // console.log(a[i])
+    }
+    draw(a,-1,-1,"green")
+    console.log('came');
+}
+function bubblesort() {
+    cnt++;
+    // console.log(slider.value)
+    
+    
+    let i = 0,j = 0,oldcnt= cnt
     var repeat = setInterval(() => {
-        if(i >= n){
-            draw(a,-1,-1)
+        console.log(cnt)
+        if(i >= n || cnt != oldcnt){
+            draw(a,-1,-1,"green")
             clearInterval(repeat);
         }
         j = j + 1
@@ -47,15 +72,37 @@ window.onload = function() {
             i = i + 1
             j = 0
         }
-        if(j + 1 < n && a[j] > a[j + 1]){
+        draw(a,j,j + 1,"red")
+        if(j + 1 < n && i < n && a[j] > a[j + 1]) {
             let temp = a[j]
             a[j] = a[j + 1]
             a[j + 1] = temp
-        }      
-        draw(a,j,j + 1)
-        if(i >= n){
-            draw(a,-1,-1)
+            draw(a,j,j + 1,"red")
+        }  
+        if(i >= n) {
+            draw(a,-1,-1,"green")
             console.log('Completed')
         }
-    }, 1000/10);
+    }, 1000/300);
 }
+
+document.getElementById('sort_button').addEventListener('click',bubblesort);
+document.getElementById('stop_button').addEventListener('click',function(){
+    cnt++;
+});
+
+// bubble sort not working
+// for(i = 0; i < n; i++){
+//     for(j = 0; j < n - i - 1; j++){
+//         if(a[j] > a[j + 1]){
+//             draw(a,j,j + 1)
+//             setTimeout(() => {
+//                 let temp = a[j]
+//                 a[j] = a[j + 1]
+//                 a[j + 1] = temp
+//                 cnt++;
+//             }, 100);
+//             draw(a,j,j + 1)
+//         }
+//     }
+// }
