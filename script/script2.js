1f6a10d32203009682551cdc08023e6fc7820c69
harshil-270 <harshiltagadiya@gmail.com>
2020-08-21 13:16:17 +0530

    Improved Visulization

--- script/script2.js
+++ script/script2.js
@@ -1,28 +1,28 @@
 var lastClicked;
 let rows = 20,cols = 40;
-var mouseIsDown = false;
+let mouseIsDown = false;
 let grid = new Array(rows);
 let data = new Array(rows);
+let cnt = 0;
 for(let i = 0; i < rows; i++){
     grid[i] = new Array(cols).fill(0);
     data[i] = new Array(cols);
 }
-
 
 let starting_point_i = 9;
 let starting_point_j = 14;
 let ending_point_i = 9;
 let ending_point_j = 24;
 
-
 class Queue
 { 
-    constructor() {
+    constructor(){
         this.items = []; 
     }
     enqueue(element){
         this.items.push(element); 
     }
+
     dequeue(){
         if(this.isEmpty()) 
             return "Underflow"; 
@@ -33,10 +33,10 @@ class Queue
             return "No elements in Queue"; 
         return this.items[0]; 
     }
-    isEmpty() {
+    isEmpty(){
         return this.items.length == 0; 
     }
-    printQueue() {
+    printQueue(){
         var str = ""; 
         for(var i = 0; i < this.items.length; i++) 
             str += this.items[i] +" "; 
@@ -107,9 +107,8 @@ function clickableGrid( rows, cols, callback ){
 }
 
 
-
-
 function clear_grid(){
+    cnt++;
     for(let i = 0; i < rows; i++){
         for(let j = 0; j < cols; j++){
             if((i == starting_point_i && j == starting_point_j) || (i == ending_point_i && j == ending_point_j));
@@ -120,8 +119,6 @@ function clear_grid(){
         }
     }
 }
-
-
 
 function printpath(path){
     console.log("bfs done. Printing path");
@@ -159,7 +156,11 @@ function printpath(path){
     curi = starting_point_i;
     curj = starting_point_j;
     let i = 0;
+    let oldcnt = cnt;
     let pathrepeater = setInterval(() => {
+        if(cnt != oldcnt){
+            clearInterval(pathrepeater)
+        }
         curi += (s[i] == "D");
         curi -= (s[i] == "U");
         curj += (s[i] == "R");
@@ -171,8 +172,6 @@ function printpath(path){
         }
     },1000/20);
 }
-
-
 
 function BFS(){
     for(let i = 0; i < rows; i++){
@@ -193,8 +192,11 @@ function BFS(){
     q.enqueue([starting_point_i,starting_point_j]);
     vis[starting_point_i][starting_point_j] = 1;
     path[starting_point_i][starting_point_j] = "1";
-
+    let oldcnt = cnt;
     let bfsrepeater = setInterval(() => {
+        if(oldcnt != cnt){
+            clearInterval(bfsrepeater)
+        }
         let p = q.front();
         q.dequeue();
         let i = p[0];
@@ -234,6 +236,76 @@ function BFS(){
     },1000/60);
 }
 
+function DFS(){    
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < cols; j++){
+            if(data[i][j].className == 'visited' || data[i][j].className == 'path'){
+                data[i][j].className = '';
+            }
+        }
+    }
+    let vis = new Array(rows);
+    let path = new Array(rows);
+    for(let i = 0; i < rows; i++){
+        vis[i] = new Array(cols).fill(0);
+        path[i] = new Array(cols).fill("0");
+    }
 
-document.getElementById('visualize_button').addEventListener('click',BFS);
+    let s = [];
+    s.push([starting_point_i,starting_point_j]);
+    vis[starting_point_i][starting_point_j] = 1;
+    path[starting_point_i][starting_point_j] = "1";
+    let oldcnt = cnt;
+    let dfsrepeater = setInterval(() => {
+        if(oldcnt != cnt){
+            clearInterval(dfsrepeater);
+        }
+        let p = s[s.length - 1];
+        s.pop();
+        let i = p[0];
+        let j = p[1];
+        
+        if(i == ending_point_i && j == ending_point_j){
+            clearInterval(dfsrepeater);
+            
+            printpath(path);
+        }
+        else if(!(i == starting_point_i && j == starting_point_j)){
+            data[i][j].className = 'visited';
+        }
+        if(j > 0 && grid[i][j - 1] === 0 && vis[i][j - 1] === 0){
+            s.push([i,j - 1]);
+            vis[i][j - 1] = 1;
+            path[i][j - 1] = "R";
+        }
+        if(j < cols - 1 && grid[i][j + 1] === 0 && vis[i][j + 1] === 0){
+            s.push([i,j + 1]);
+            vis[i][j + 1] = 1;
+            path[i][j + 1] = "L";
+        }
+        if(i < rows - 1 && grid[i + 1][j] === 0 && vis[i + 1][j] === 0){
+            s.push([i + 1,j]);
+            vis[i + 1][j] = 1;
+            path[i + 1][j] = "U";
+        }
+        if(i > 0 && grid[i - 1][j] === 0 && vis[i - 1][j] === 0){
+            s.push([i - 1,j]);
+            vis[i - 1][j] = 1;
+            path[i - 1][j] = "D";
+        }
+    },1000/60);
+}
+
+function algorithm_caller(){
+    type = document.getElementById('algorithm_type').value
+    cnt++;
+    if(type == 'dfs'){
+        DFS();
+    }
+    else if(type == 'bfs'){
+        BFS();
+    }
+}
+
+document.getElementById('visualize_button').addEventListener('click',algorithm_caller);
 document.getElementById('clear_button').addEventListener('click',clear_grid);
